d7a88b8ad71da0d903aafacabce07bd9704fc247
Habu-Kagumba <habukagumba@gmail.com>
2016-02-07 18:05:37 +0300

    Conform to standard js style.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-import test from "tape"
-import productShowcase from "../src"
+import test from 'tape'
+import productShowcase from '../src'
 
-test("productShowcase", (t) => {
+test('productShowcase', (t) => {
   t.plan(1)
-  t.equal(true, productShowcase(), "return true")
+  t.equal(true, productShowcase(), 'return true')
 })
